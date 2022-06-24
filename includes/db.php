ad98cf3010e25c37c0544502e775f3ed12e1f2b2
KiraBelak <54724730+KiraBelak@users.noreply.github.com>
2022-06-24 00:13:02 -0600

    first commit

---
+++ includes/db.php
@@ -0,0 +1,42 @@
+<?php
+
+class DB{
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct(){
+        $this->host     = 'kafemabd-instance-1.c8afvo7lm7ku.us-east-1.rds.amazonaws.com';
+        $this->db       = 'flask_login';
+        $this->user     = 'root';
+        $this->password = "prueba123";
+        $this->charset  = 'utf8mb4';
+    }
+
+    function connect(){
+    
+        try{
+            
+            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+    
+            return $pdo;
+
+        }catch(PDOException $e){
+            print_r('Error connection: ' . $e->getMessage());
+        }   
+    }
+}
+
+
+
+
+
+
+?>
